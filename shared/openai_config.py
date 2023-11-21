39129529b7203def74bcd7370148405ce1d713bc
Kevin <kevin.armengol@gmail.com>
2023-11-21 14:55:08 -0500

    Added local env files

--- shared/openai_config.py
+++ shared/openai_config.py
@@ -1,6 +1,13 @@
 from shared.settings import Settings
 from openai import OpenAI
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+
 def get_openai_client():
     settings = Settings()
     return OpenAI(api_key=settings.OPENAI_API_KEY)
